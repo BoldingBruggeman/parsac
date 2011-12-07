16ef4fd2a0ce8b4cc20c9021afb19df3811028f6
Jorn Bruggeman <jorn.bruggeman@earth.ox.ac.uk>
2011-12-07 11:50:02 +0000

    updated create_database.py to ask for root pw and confirmation

--- create_database.py
+++ create_database.py
@@ -1,11 +1,18 @@
 import MySQLdb,sys
 import mysqlinfo
 
-print 'Database creation disabled for security reasons.'
-sys.exit(1)
+pw = raw_input('Root password for MySQL server %s: ' % mysqlinfo.host)
 
-db = MySQLdb.connect(host=mysqlinfo.host, user='root',passwd='1r3z2g6')
+db = MySQLdb.connect(host=mysqlinfo.host, user='root',passwd=pw)
 c = db.cursor()
+
+print 'Connected to MySQL server %s.' % mysqlinfo.host
+resp = ''
+while resp not in ('y','n'):
+    resp = raw_input('Are you sure you want to delete any existing database "%s" and create a new one? (y/n): ' % mysqlinfo.database)
+if resp=='n':
+    print 'Database creation cancelled.'
+    sys.exit(0)
 
 #c.execute('ALTER TABLE `runs` ADD COLUMN `job` INT AFTER `time`;')
 #c.execute('ALTER TABLE `results` MODIFY `parameters` VARCHAR(500);')
@@ -16,7 +23,7 @@ c.execute('CREATE DATABASE `%s`;' % mysqlinfo.database)
 
 # Create users and grant minimum permissions
 c.execute('CREATE USER \'%s\'@\'%\' IDENTIFIED BY \'%s\';' % (mysqlinfo.runuser,mysqlinfo.runpassword))
-c.execute('CREATE USER \'%s\'@\'localhost\' IDENTIFIED BY \'%s\';' % (mysqlinfo.viewuser,mysqlinfo.viewpassword)
+c.execute('CREATE USER \'%s\'@\'localhost\' IDENTIFIED BY \'%s\';' % (mysqlinfo.viewuser,mysqlinfo.viewpassword))
 c.execute('GRANT SELECT ON `%s`.* TO \'%s\'@\'localhost\';' % (mysqlinfo.database,mysqlinfo.viewuser))
 c.execute('GRANT INSERT ON `%s`.* TO \'%s\'@\'%\';' % (mysqlinfo.database,mysqlinfo.runuser))
 
