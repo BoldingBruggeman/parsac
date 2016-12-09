c04c5578b60c66abe259c4653f9fb6c8cf639e48
Karsten Bolding <karsten@bolding-bruggeman.com>
2016-12-09 17:07:27 +0100

    first steps towards wheel

---
+++ README.md
@@ -0,0 +1,24 @@
+# AutoCalibration Python
+
+Python-based auto-calibration tool. Expect configuration of model parameters through namelist or yaml files and model output in the form of NetCDF files.
+
+
+## Howto wheel
+
+### To make a wheel
+
+pip wheel .
+
+
+### To install a wheel
+
+pip install <--upgrade> acpy-0.1-py2-none-any.whl
+
+Add options -v -v -v to see where files are installed
+
+Add --user to install in default *user* directory
+
+
+### To work with a wheel in edit mode
+
+pip2 install -e . --user --upgrade
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='acpy',
+      version='0.1',
+      description='AutoCalibration tool in Python',
+      long_description=readme(),
+      url='http://github.com/BoldingBruggeman/acpy',
+      author='Jorn Bruggeman',
+      author_email='jorn@bolding-bruggeman.com',
+      license='GPL',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Topic :: Numerical Models :: Calibration Tools',
+          'License :: OSI Approved :: GPL License',
+          'Programming Language :: Python :: 2.7',
+      ],
+#      scripts=['bin/acpy.py.py', 'bin/animate_2d.py', 'plotbest.py', 'plot.py'],
+      scripts=['bin/acpy.py',],
+      packages=['acpy'],
+      include_package_data=True,
+      zip_safe=False)
