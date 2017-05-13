c21250c738c43836210d5b188d4f3215a772ddbb
Karsten Bolding <karsten@bolding-bruggeman.com>
2017-05-13 12:08:15 +0200

    license: default installation does not contain a license file

--- acpy/license.py
+++ acpy/license.py
@@ -4,6 +4,7 @@ import os
 import argparse
 import ConfigParser
 
+have_license = False
 user = None
 key = None
 parallel = None
@@ -14,32 +15,41 @@ license_file  = os.path.join(path, 'license.txt')
 config = ConfigParser.SafeConfigParser()
 
 def read():
-    try:
-        config.read(license_file)
-        user = config.get('User','user')
-        key = config.get('Key','key')
-        parallel = config.get('Features','parallel')
-    except ConfigParser.ParsingError, err:
-        print 'Could not parse:', err
+    global have_license
+    global user
+    global key
+    global parallel
+    if os.path.isfile(license_file):
+        try:
+            config.read(license_file)
+            user = config.get('User','user')
+            key = config.get('Key','key')
+            parallel = config.get('Features','parallel')
+            have_license = True
+        except ConfigParser.ParsingError, err:
+            print 'Could not parse:', err
 
 #def write():
 #    print('Write cfg-file')
 
 def print_c():
-   for section_name in config.sections():
-       print 'Section:', section_name
-       for name, value in config.items(section_name):
-           print '  %s = %s' % (name, value)
-       print
+    if have_license:
+        for section_name in config.sections():
+            print 'Section:', section_name
+            for name, value in config.items(section_name):
+                print '  %s = %s' % (name, value)
+    else:
+        print("No license file found!")
 
 def main(args):
     read()
     print_c()
 
 if __name__ == "__main__":
-    print('Reading license from:')
-    print('   '+license_file)
-    print
+    if have_license:
+        print('Reading license from:')
+        print('   '+license_file)
+        print
     args = None
     main(args)
 
