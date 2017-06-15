f7018f250c2bf4c0a9833b540ad304002ca24d56
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2017-06-15 09:53:42 +0100

    use xml directory as basis for locating function module

--- acpy/job/function.py
+++ acpy/job/function.py
@@ -1,4 +1,5 @@
 import sys
+import os.path
 
 import shared
 
@@ -11,15 +12,14 @@ class Job(shared.Job):
             raise Exception('The root node must contain a single "function" element.')
         with shared.XMLAttributes(element, 'the function element') as att:
             self.module, self.name = att.get('name', unicode).rsplit('.', 1)
-            self.path = att.get('path', unicode, default=root)
+            self.path = os.path.join(root, att.get('path', unicode, default='.'))
         self.parameter_names = self.getParameterNames()
         self.function = None
 
     def evaluateFitness(self, parameter_values):
         if self.function is None:
-            if self.path is not None:
-                sys.path.append(self.path)
+            sys.path.append(self.path)
             module = __import__(self.module)
             self.function = getattr(module, self.name)
-        info = dict(zip(self.parameter_names, parameter_values))
-        return self.function(**info)
+        parameter2value = dict(zip(self.parameter_names, parameter_values))
+        return self.function(**parameter2value)
