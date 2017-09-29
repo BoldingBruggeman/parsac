78a383d8e07bb2efb54911cad13bf515f8415490
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2017-09-29 10:59:29 +0100

    support additional SALib samplers

--- acpy/sensitivity.py
+++ acpy/sensitivity.py
@@ -23,6 +23,7 @@ def configure_argument_parser(parser):
     parser.add_argument('-i', '--interactive', action='store_true', help='Whether to allow for user interaction (input from stdin) when making decisions')
     parser.add_argument('--create_dirs', type=str, help='Create setup directories under the specified root.')
     parser.add_argument('--format', type=str, help='Format for subdirectory name (only in combination with --create_dirs).', default='%04i')
+    parser.add_argument('--sampler', type=str, choices=('fast', 'saltelli', 'morris', 'latin', 'ff'), help='Sampling method. Options: fast, saltelli, morris, latin, ff.', default='saltelli')
 
 def main(args):
     assert args.create_dirs is not None, 'Currently --create_dirs argment must be provided'
@@ -32,31 +33,47 @@ def main(args):
     print 'Reading configuration from %s...' % args.xmlfile
     current_job = job.fromConfigurationFile(args.xmlfile)
 
-    with open(args.xmlfile) as f:
-        xml = f.read()
-    reporter = report.fromConfigurationFile(args.xmlfile, xml, allowedtransports=args.transport)
-
-    # Configure result reporter
-    reporter.interactive = args.interactive
-    if args.reportinterval is not None:
-        reporter.timebetweenreports = args.reportinterval
-
     names = current_job.getParameterNames()
     minpar, maxpar = current_job.getParameterBounds()
-    try:
-        import SALib.sample.saltelli
-        problem = {'num_vars': len(names),
-                   'names': names,
-                   'bounds': list(zip(minpar, maxpar))
-        }
-        parameters = SALib.sample.saltelli.sample(problem, args.N)
+
+    if args.sampler == 'saltelli':
+        from SALib.sample.saltelli import sample
+    elif args.sampler == 'morris':
+        from SALib.sample.morris import sample
+    elif args.sampler == 'fast':
+        from SALib.sample.fast_sampler import sample
+    elif args.sampler == 'latin':
+        from SALib.sample.latin import sample
+    elif args.sampler == 'ff':
+        from SALib.sample.ff import sample
+    SAlib_problem = {'num_vars': len(names),
+                     'names': names,
+                     'bounds': list(zip(minpar, maxpar))
+    }
+    parameters = sample(SAlib_problem, args.N)
+
+    if args.create_dirs:
+        # Only create setup directories
         assert isinstance(current_job, job.program.Job)
         for i in xrange(parameters.shape[0]):
             current_job.simulationdir = os.path.join(args.create_dirs, args.format % i)
             current_job.initialized = False
             current_job.prepareDirectory(parameters[i, :])
-    finally:
-        reporter.finalize()
+    else:
+        # Run the actual sensitivity analysis (yet to be implemented)
+        with open(args.xmlfile) as f:
+            xml = f.read()
+        reporter = report.fromConfigurationFile(args.xmlfile, xml, allowedtransports=args.transport)
+
+        # Configure result reporter
+        reporter.interactive = args.interactive
+        if args.reportinterval is not None:
+            reporter.timebetweenreports = args.reportinterval
+
+        try:
+            pass
+        finally:
+            reporter.finalize()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
