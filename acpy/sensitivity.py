41d616a7b642dabdfdec11554e2d718a4ec4d80b
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2018-11-23 15:18:16 +0000

    ensure backward compatibility with jobs that have singular target

--- acpy/sensitivity.py
+++ acpy/sensitivity.py
@@ -227,7 +227,8 @@ def main(args):
             sys.exit(2)
         X, Y = job_info['X'], job_info['Y']
         Y.shape = (X.shape[0], -1)
-        for itarget, (expression, ncpath) in enumerate(current_job.targets):
+        targets = getattr(current_job, 'targets', [getattr(current_job, 'target', None)]):
+        for itarget, (expression, ncpath) in enumerate(targets):
             print('Target %s (read from %s):' % (expression, ncpath))
             sensitivities = analyze(SAlib_problem, args, job_info['sample_args'], X, Y[:, itarget])
             if args.select is not None:
