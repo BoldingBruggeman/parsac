ac00d42f1ec3aa3198d08c668885e516c91614a6
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2018-11-23 16:48:35 +0000

    removed --squash from subtree

--- admin/pull_optimize.bat
+++ admin/pull_optimize.bat
@@ -1,2 +1,2 @@
 set HOME=%USERPROFILE%
-git subtree pull --prefix acpy/optimize git@bitbucket.org:jbruggeman/optimize.git master --squash
+git subtree pull --prefix acpy/optimize git@bitbucket.org:jbruggeman/optimize.git master
