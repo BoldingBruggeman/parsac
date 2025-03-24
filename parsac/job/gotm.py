5b636e009af1f5c55ea9912b4315671011b70ac1
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2025-03-24 16:04:13 +0000

    gotm: support executable from PATH

--- parsac/job/gotm.py
+++ parsac/job/gotm.py
@@ -192,11 +192,13 @@ class Simulation(core.Runner):
             raise Exception(f"GOTM setup directory {setup_dir} does not exist.")
         self.setup_dir = setup_dir
         super().__init__(f"gotm({setup_dir})")
-        if not use_shell:
-            executable = (setup_dir / Path(executable)).resolve()
-            if not executable.is_file():
+        abs_exe = (setup_dir / Path(executable)).resolve()
+        if abs_exe.is_file():
+            self.executable = abs_exe
+        else:
+            if shutil.which(executable) is None:
                 raise Exception(f"Executable {executable} not found.")
-        self.executable = executable
+            self.executable = executable
         if work_dir is not None:
             work_dir = Path(work_dir)
         self.work_dir = work_dir
