d3ba11e8f86724cb22fc5b6a837cd702787e07d1
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2025-06-05 09:08:16 +0100

    test tweaks

--- test/test_infer.py
+++ test/test_infer.py
@@ -9,9 +9,12 @@ import numpy as np
 class TestInfer(unittest.TestCase):
     def test_type(self):
         exp = parsac.optimize.Optimization()
-        dummy1 = exp.add_parameter("test", 0.0, 1.0)
+        dummy1 = exp.add_parameter("dummy1", 0.0, 1.0)
         parsac.core.Parameter("dummy2").infer(lambda x: np.exp(x) * 0.00024, dummy1)
-        for k, v in exp.unpack_parameters([0.5]).items():
+        exp.add_parameter("dummy3", 0.0, 1.0)
+        for k, v in exp.unpack_parameters([0.5, np.exp(1.0)]).items():
+            with self.subTest(k=k):
+                self.assertEqual(type(v), float)
             self.assertEqual(type(v), float)
 
 
