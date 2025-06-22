803d4e90b85315e510c5faffadd599bc7f44b785
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2025-06-22 14:01:53 +0100

    add sphinx+readthedocs config

---
+++ docs/source/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "parsac"
+copyright = "2025, Bolding & Bruggeman ApS"
+author = "Bolding & Bruggeman ApS"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.apidoc",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+
+html_static_path = ["_static"]
+
+napoleon_include_special_with_doc = True
+
+# -- Extension configuration -------------------------------------------------
+autoclass_content = "both"
+
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
+
+apidoc_modules = [
+    {"path": "../../parsac", "destination": "api", "separate_modules": True},
+]
---
+++ docs/source/index.rst
@@ -0,0 +1,21 @@
+.. parsac documentation master file, created by
+   sphinx-quickstart on Sun Jun 22 13:13:03 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+parsac documentation
+====================
+
+.. toctree::
+   :maxdepth: 3
+   :caption: Contents:
+
+   api/parsac
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
